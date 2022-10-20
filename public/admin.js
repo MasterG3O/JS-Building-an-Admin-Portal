b922bf748491a5a89c7eee4fb4586b65e2b8f7b7
Geordanis Mondeja <gmondeja32@gmail.com>
2022-10-20 13:30:01 -0700

    Updated code

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,17 @@
 
 // Your Code Here
+
+async function saveShinobi(){
+    let inputValue = document.getElementById("shinobi-input").value
+    console.log("button clicked")
+    let response = await fetch('http://localhost:3000/updateBook',{
+        method: "PATCH",
+        headers: {
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify({
+            "id": 1,
+            "quantify": inputValue
+        })
+    })
+}
